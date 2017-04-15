764fca0a4f665cb5a87ca09b60285c223ad57cce
tomine3 <sd70126@gmail.com>
2017-04-15 07:47:23 +0000

    It will be able to save room and group userID

--- recieve.php
+++ recieve.php
@@ -21,11 +21,21 @@ function recieve_main_proc(){
             break;
         case 'join':
         case 'follow':
-            registId($jsonObj->{"events"}[0]->{"source"}->{"userId"}, $jsonObj->{"events"}[0]->{"source"}->{"type"}, $event_type = $jsonObj->{"events"}[0]->{"timestamp"});
+            if($jsonObj->{"events"}[0]->{"source"}->{"type"} === "user")
+                registId($jsonObj->{"events"}[0]->{"source"}->{"userId"}, $jsonObj->{"events"}[0]->{"source"}->{"type"}, $event_type = $jsonObj->{"events"}[0]->{"timestamp"});
+            elseif($jsonObj->{"events"}[0]->{"source"}->{"type"} === "group")
+                registId($jsonObj->{"events"}[0]->{"source"}->{"groupId"}, $jsonObj->{"events"}[0]->{"source"}->{"type"}, $event_type = $jsonObj->{"events"}[0]->{"timestamp"});
+            elseif($jsonObj->{"events"}[0]->{"source"}->{"type"} === "room")
+                registId($jsonObj->{"events"}[0]->{"source"}->{"roomId"}, $jsonObj->{"events"}[0]->{"source"}->{"type"}, $event_type = $jsonObj->{"events"}[0]->{"timestamp"});
             break;
         case 'leave':
         case 'unfollow':
-            deleteId($jsonObj->{"events"}[0]->{"source"}->{"userId"}, $jsonObj->{"events"}[0]->{"source"}->{"type"}, $event_type = $jsonObj->{"events"}[0]->{"timestamp"});
+            if($jsonObj->{"events"}[0]->{"source"}->{"type"} === "user")
+                deleteId($jsonObj->{"events"}[0]->{"source"}->{"userId"}, $jsonObj->{"events"}[0]->{"source"}->{"type"}, $event_type = $jsonObj->{"events"}[0]->{"timestamp"});
+            elseif($jsonObj->{"events"}[0]->{"source"}->{"type"} === "group")
+                registId($jsonObj->{"events"}[0]->{"source"}->{"groupId"}, $jsonObj->{"events"}[0]->{"source"}->{"type"}, $event_type = $jsonObj->{"events"}[0]->{"timestamp"});
+            elseif($jsonObj->{"events"}[0]->{"source"}->{"type"} === "room")
+                registId($jsonObj->{"events"}[0]->{"source"}->{"roomId"}, $jsonObj->{"events"}[0]->{"source"}->{"type"}, $event_type = $jsonObj->{"events"}[0]->{"timestamp"});
             break;
         default:
             // code...
